3c07f9581025facc29687ffcb77c98d709821d4d
thilinasandamal <thilinasandamal61@gmail.com>
2025-04-28 00:53:34 +0530

    updated main and css.app

--- branwavee/src/App.jsx
+++ branwavee/src/App.jsx
@@ -4,6 +4,12 @@ import Home from './Home/Home'
 import Login from './pages/login/Login'
 import { Route, Routes } from 'react-router-dom'
 
+import MessageList from './Component/MessageList'
+import ChatWindow from './Component/ChatWindow'
+
+import { UserProfile } from './Home/UserProfile'
+
+
 const App = () => {
   return (
     <Routes>
@@ -13,6 +19,12 @@ const App = () => {
       {/* Protected routes inside LayOut */}
       <Route path="/" element={<LayOut />}>
         <Route path="Dash" element={<Home />} />
+
+        <Route path="/messages" element={<MessageList />} />
+        <Route path="/message/:userId" element={<ChatWindow />} />
+
+        <Route path="profile"element={<UserProfile />} />
+
       </Route>
     </Routes>
   )
